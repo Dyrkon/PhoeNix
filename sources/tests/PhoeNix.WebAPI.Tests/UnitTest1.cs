b455bb0216394f2fcd67a3a20f14b1bedc57985f
Matěj Mudra <dyrkon603@gmail.com>
2024-10-07 10:23:47 +0200

    Resolve #3: Add project structure and starting projects

---
+++ sources/tests/PhoeNix.WebAPI.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PhoeNix.WebAPI.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
