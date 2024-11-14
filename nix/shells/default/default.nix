d1a3d45641e5408c40ebf68e4e85d69149dba082
Matej Mudra <dyrkon603@gmail.com>
2024-11-14 21:57:14 +0100

    fix: correct chrome path for playwright

--- nix/shells/default/default.nix
+++ nix/shells/default/default.nix
@@ -9,10 +9,15 @@
   inherit (pkgs) stdenv;
   solution = pkgs.${namespace}.solution;
   chrome-version = "1091";
-  chrome-path = if stdenv.isLinux then 
-    "${pkgs.playwright-driver.browsers}/chromium-${chrome-version}/chrome-linux/chrome" 
-  else 
-    "${pkgs.playwright-driver.browsers}/chromium-${chrome-version}/chrome-mac/Chromium.app/Contents/MacOS/Chromium";
+  chrome-path =
+    if stdenv.isLinux
+    then "${pkgs.playwright-driver.browsers}/chromium-${chrome-version}/chrome-linux/chrome"
+    else "${pkgs.playwright-driver.browsers}/chromium-${chrome-version}/chrome-mac/Chromium.app/Contents/MacOS/Chromium";
+
+  nodePath =
+    if stdenv.isLinux
+    then "sources/tests/PhoeNix.WebAPP.Tests/bin/Debug/net8.0/.playwright/node/linux-x64/node"
+    else "sources/tests/PhoeNix.WebAPP.Tests/bin/Debug/net8.0/.playwright/node/mac/node";
 
   shell = mkShell {
     packages =
@@ -23,15 +28,18 @@
         pkgs.nixos-anywhere
         pkgs.alejandra
         pkgs.nodejs
-        pkgs.playwright-driver.browsers];
+        pkgs.playwright-driver.browsers
+      ];
     shellHook = ''
       export PLAYWRIGHT_NODEJS_PATH=${pkgs.nodejs}/bin/node;
-      export PLAYWRIGHT_LAUNCH_OPTIONS_EXECUTABLE_PATH=${pkgs.playwright-driver.browsers}/chromium-1091/chrome-linux/chrome;
+      export PLAYWRIGHT_LAUNCH_OPTIONS_EXECUTABLE_PATH=${chrome-path};
       export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
       export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
       export DOTNET_ROOT=${solution.dotnet-runtime}
       export LD_LIBRARY_PATH="${solution.dotnet-sdk.icu}/lib:${pkgs.lib.makeLibraryPath solution.runtimeDeps}"
       unset DOTNET_SKIP_FIRST_TIME_EXPERIENCE
+      rm ./${nodePath}
+      ln -s ${pkgs.nodejs}/bin/node ./${nodePath}
     '';
   };
 in
