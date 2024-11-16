35ba6d486eefbd716d479f34ef27f94fdac0c52b
Matěj Mudra <54201410+Dyrkon@users.noreply.github.com>
2024-11-16 11:28:56 +0100

    Fixes #8 - Add github acitons to test project packages(#12)
    * Fixes #8 - Add github acitons to test project packages
    
    * fix: correct chrome path for playwright
    
    * feat: add test pipeline
    
    * fix: write playwright wrapper

--- nix/shells/default/default.nix
+++ nix/shells/default/default.nix
@@ -6,12 +6,22 @@
   mkShell,
   ...
 }: let
+  inherit (pkgs) stdenv;
   solution = pkgs.${namespace}.solution;
+
   shell = mkShell {
     packages =
       solution.runtimeDeps
-      ++ [solution.dotnet-sdk solution.dotnet-runtime pkgs.nixos-anywhere pkgs.alejandra];
+      ++ [
+        solution.dotnet-sdk
+        solution.dotnet-runtime
+        pkgs.nixos-anywhere
+        pkgs.alejandra
+        pkgs.nodejs
+        pkgs.powershell
+      ];
     shellHook = ''
+      export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
       export DOTNET_ROOT=${solution.dotnet-runtime}
       export LD_LIBRARY_PATH="${solution.dotnet-sdk.icu}/lib:${pkgs.lib.makeLibraryPath solution.runtimeDeps}"
       unset DOTNET_SKIP_FIRST_TIME_EXPERIENCE
